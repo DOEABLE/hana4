e2402591e89c23b7f32111093126e48344af54e2
KIMDOHEE <kimdh090588@gmail.com>
2024-09-25 13:07:06 +0900

    item

--- rbvite/src/components/My.tsx
+++ rbvite/src/components/My.tsx
@@ -1,46 +1,19 @@
-import { FaPlus, FaTrashCan } from 'react-icons/fa6';
+import { FaPlus } from 'react-icons/fa6';
 import Login from './Login.tsx';
 import Profile from './Profile.tsx';
 import Button from './atoms/Button.tsx';
-import { FormEvent, useRef, useState } from 'react';
-import { FaRedo, FaSave } from 'react-icons/fa';
+import { useRef, useState } from 'react';
 import { useSession } from '../hooks/session-context.tsx';
-import { useCounter } from '../hooks/counter-hook.tsx';
+import Item from './Item.tsx';
 
 export default function My() {
-  const { session, removeCartItem, addCartItem } = useSession();
-  const { plusCount } = useCounter();
-  const [isEditing, setIsEditing] = useState(false);
+  const { session } = useSession();
+
+  const [isAdding, setIsAdding] = useState(false);
   const logoutButtonRef = useRef<HTMLButtonElement>(null);
-  const nameRef = useRef<HTMLInputElement>(null);
-  const priceRef = useRef<HTMLInputElement>(null);
 
-  const toggleEditing = () => {
-    setIsEditing((pre) => !pre);
-    plusCount();
-  };
-  const removeItem = (id: number) => {
-    if (confirm('정말 삭제하시겠습니까?')) {
-      removeCartItem(id);
-    }
-  };
-  const saveItem = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const name = nameRef.current?.value;
-    const price = priceRef.current?.value;
-    //console.log(name, price);
-    if (!name) {
-      alert('상품명을 입력하세요.');
-      return nameRef.current?.focus();
-    } else if (!price) {
-      alert('금액을 입력하세요.');
-      return priceRef.current?.focus();
-    }
-
-    addCartItem(name, +price);
-    nameRef.current.value = '';
-    priceRef.current.value = '';
-    nameRef.current.focus();
+  const toggleAdding = () => {
+    setIsAdding((pre) => !pre);
   };
 
   return (
@@ -56,51 +29,24 @@ export default function My() {
         <Login />
       )}
 
-      <ul className='my-3 w-1/2 border p-3'>
+      <ul className='my-3 w-2/3 border p-3'>
         {session.cart?.length ? (
-          session.cart.map(({ id, name, price }) => (
-            <li className='flex justify-between' key={id}>
-              <strong>
-                {id}. {name}
-                <small className='text-gray-500'>
-                  ({price.toLocaleString()}원)
-                </small>
-              </strong>
-              <button
-                className='btn btn-danger px-1 py-0'
-                onClick={() => removeItem(id)}
-              >
-                <FaTrashCan />
-              </button>
+          session.cart.map((item) => (
+            <li key={item.id}>
+              <Item item={item} />
             </li>
           ))
         ) : (
-          <li className='text-slate-500'>장바구니가 비었습니다.</li>
+          <li className='text-slate-500'>There is no items.</li>
         )}
         <li className='mt-3 text-center'>
-          {isEditing ? (
-            <form onSubmit={saveItem} className='mt-3 flex gap-3'>
-              <input
-                ref={nameRef}
-                type='text'
-                placeholder='이름을 입력하세요.'
-                className='inp'
-              />
-              <input
-                ref={priceRef}
-                type='number'
-                placeholder='가격을 입력하세요.'
-                className='inp'
-              />
-              <Button type='reset' onClick={toggleEditing}>
-                <FaRedo />
-              </Button>
-              <Button type='submit' variant='btn-primary'>
-                <FaSave />
-              </Button>
-            </form>
+          {isAdding ? (
+            <Item
+              item={{ id: 0, name: '', price: 0 }}
+              toggleAdding={toggleAdding}
+            />
           ) : (
-            <Button onClick={toggleEditing}>
+            <Button onClick={toggleAdding}>
               <FaPlus /> Add Item
             </Button>
           )}
--- rbvite/src/hooks/session-context.tsx
+++ rbvite/src/hooks/session-context.tsx
@@ -9,7 +9,7 @@ import {
 import { LoginHandler } from '../components/Login';
 
 const SampleSession = {
-  loginUser: { id: 1, name: 'Hong' },
+  loginUser: { id: 1, name: '홍길동' },
   cart: [
     { id: 100, name: '라면', price: 3000 },
     { id: 101, name: '컵라면', price: 2000 },
@@ -18,7 +18,7 @@ const SampleSession = {
 };
 
 type LoginUser = typeof SampleSession.loginUser;
-type CartItem = { id: number; name: string; price: number };
+export type CartItem = { id: number; name: string; price: number };
 export type Session = { loginUser: LoginUser | null; cart: CartItem[] };
 
 const contextInitValue = {
@@ -29,28 +29,32 @@ const contextInitValue = {
   },
   removeCartItem: (id: number) => console.log(id),
   addCartItem: (name: string, price: number) => console.log(name, price),
+  editCartItem: (item: CartItem) => console.log(item),
   loginRef: createRef<LoginHandler>(),
 };
 
 type SessionContextProps = Omit<typeof contextInitValue, 'session'> & {
   session: Session;
 };
+
 const SessionContext = createContext<SessionContextProps>(contextInitValue);
 
 export const SessionProvider = ({ children }: PropsWithChildren) => {
   const [session, setSession] = useState<Session>(SampleSession);
+
   const loginRef = useRef<LoginHandler>(null);
 
   const logout = () => setSession({ ...session, loginUser: null });
 
   const login = (id: number, name: string) => {
     if (!id) {
-      alert('사용자 ID를 입력하시오.');
+      alert('사용자 ID를 입력하세요!');
       console.log('>>>', loginRef.current);
       return loginRef.current?.focus('id');
     }
+
     if (!name) {
-      alert('사용자 ID를 입력하시오.');
+      alert('Name을 입력하세요!');
       return loginRef.current?.focus('name');
     }
 
@@ -71,22 +75,32 @@ export const SessionProvider = ({ children }: PropsWithChildren) => {
       cart: session.cart.filter(({ id }) => id !== toRemoveId),
     });
   };
+
+  const editCartItem = (item: CartItem) => {
+    setSession({
+      ...session,
+      cart: session.cart.map((oldItem) =>
+        oldItem.id === item.id ? item : oldItem
+      ),
+    });
+  };
+
   return (
-    <>
-      <SessionContext.Provider
-        value={{
-          session,
-          logout,
-          login,
-          addCartItem,
-          removeCartItem,
-          loginRef,
-        }}
-      >
-        {children}
-      </SessionContext.Provider>
-    </>
+    <SessionContext.Provider
+      value={{
+        session,
+        logout,
+        login,
+        removeCartItem,
+        addCartItem,
+        editCartItem,
+        loginRef,
+      }}
+    >
+      {children}
+    </SessionContext.Provider>
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useSession = () => useContext(SessionContext);
