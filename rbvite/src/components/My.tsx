2a4d2a6adfd3c649f7d84444010cb569909f3f9f
KIMDOHEE <kimdh090588@gmail.com>
2024-09-30 16:19:25 +0900

    cart

--- rbvite/src/components/My.tsx
+++ rbvite/src/components/My.tsx
@@ -2,33 +2,36 @@ import { FaPlus } from 'react-icons/fa6';
 import Login from './Login.tsx';
 import Profile from './Profile.tsx';
 import Button from './atoms/Button.tsx';
-import { useEffect, useLayoutEffect, useMemo, useRef } from 'react';
+import { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import { useSession } from '../hooks/session-context.tsx';
 import Item from './Item.tsx';
 import useToggle from '../hooks/toggle.ts';
-import { useTimeout } from '../hooks/timer.ts';
+import { useDebounce, useTimeout } from '../hooks/timer.ts';
+import { FaSearch } from 'react-icons/fa';
 
 export default function My() {
   const { session, toggleReloadSession } = useSession();
   const logoutButtonRef = useRef<HTMLButtonElement>(null);
 
-  //   const [isAdding, setIsAdding] = useState(false);
-  //   const toggleAdding = () => {
-  //     setIsAdding((pre) => !pre);//기존 값을 반대로 토글
-  //   };
-  const [isAdding, toggleAdding] = useToggle();
-  /*
-  useEffect(() => {
-     const abortController = new AbortController();
-    fetch('/data/sample.json', { signal })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('data>>', data);
-      })
-      .catch((error) => console.error(error));
+  // const [isAdding, setIsAdding] = useState(false);
+  // const toggleAdding = () => {
+  //   setIsAdding((pre) => !pre);
+  // };
 
-    return () => abortController.abort('clean-up!');
-  }, []);*/
+  const [isAdding, toggleAdding] = useToggle();
+
+  const [, toggleSearch] = useToggle();
+  const searchRef = useRef<HTMLInputElement>(null);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useDebounce(
+    () => {
+      console.log('useDebounce.search>>', searchRef.current?.value);
+      setSearchTerm(searchRef.current?.value || '');
+    },
+    2000,
+    [searchRef.current?.value]
+  );
 
   const totalPrice = useMemo(
     () => session.cart.reduce((acc, item) => acc + item.price, 0),
@@ -37,14 +40,42 @@ export default function My() {
 
   const dcPrice = useMemo(() => totalPrice * 0.1, [totalPrice]);
 
-  useLayoutEffect(() => {}, [totalPrice]);
+  useLayoutEffect(() => {
+    // console.log('$$$$$$$$$$$$$$$$', totalPrice);
+  }, [totalPrice]);
 
   let xxx = 0;
+  // useEffect(() => {
+  //   console.log('*******22');
+  //   // alert('login plz...');
+
+  //   return () => console.log('unmount22!!');
+  // }, []);
   useTimeout(() => {
     xxx++;
   }, 1000);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    // const abortController = new AbortController();
+    // const { signal } = abortController;
+    // (async function () {
+    //   try {
+    //     const data = await fetch('/data/sample.json', { signal }).then((res) =>
+    //       res.json()
+    //     );
+    //     console.log('My.data>>', data);
+    //   } catch (error) {
+    //     console.error('Error>>', error);
+    //   }
+    // })();
+    // fetch('/data/sample.json', { signal })
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log('data>>', data);
+    //   })
+    //   .catch((error) => console.error('Error>>', error));
+    // return () => abortController.abort('Clean-up in My!');
+  }, []);
 
   return (
     <>
@@ -59,7 +90,33 @@ export default function My() {
         <Login />
       )}
 
-      <ul className='my-3 w-2/3 border p-3'>
+      <div className='W-2/3 border p-3'>
+        <div className='item-center flex gap-2'>
+          <FaSearch />
+          <input
+            type='text'
+            placeholder='찾을 품목을 검색하세요.'
+            value={searchTerm}
+            onChange={toggleSearch} // 검색어 변경 시 상태 업데이트
+            className='inp'
+          />
+        </div>
+        <ul className='mt-3 px-3'>
+          {session.cart?.length > 0 ? (
+            session.cart
+              .filter(({ name }) => name.includes(searchTerm))
+              .map((item) => (
+                <li key={item.id}>
+                  <Item item={item} />
+                </li>
+              ))
+          ) : (
+            <li>No results found</li>
+          )}
+        </ul>
+      </div>
+
+      <ul className='mt-3 w-2/3 border p-3'>
         {session.cart?.length ? (
           session.cart.map((item) => (
             <li key={item.id}>
@@ -86,7 +143,7 @@ export default function My() {
         <span>*총액: {totalPrice.toLocaleString()}원</span>
         <span>*할인: {dcPrice.toFixed(0).toLocaleString()}원</span>
       </div>
-      <Button onClick={toggleReloadSession}>ReloadSession</Button>
+      <Button onClick={toggleReloadSession}>Reload Session</Button>
     </>
   );
 }
